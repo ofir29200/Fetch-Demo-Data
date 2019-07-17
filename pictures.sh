49be39aa33e97e470e37f96b09a42b9f68605d39
Bhushan Shah <bhush94@gmail.com>
2019-07-17 16:11:27 +0530

    Add some example pictures for koko/pix

---
+++ pictures.sh
@@ -0,0 +1,4 @@
+mkdir -p $1/Pictures/
+wget -c https://unsplash.com/photos/IOVgDvX7tDE/download?force=true -O $1/Pictures/wildlife-unsplash.jpg
+wget -c https://unsplash.com/photos/OZn5GjutYrg/download?force=true -O $1/Pictures/road-unsplash.jpg
+wget -c https://unsplash.com/photos/r7aGQ0kifA0/download?force=true -O $1/Pictures/bird-unsplash.jpg
