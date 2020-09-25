2fcc33c871d3bc3fb5514b2a6a15bbadbec64e92
Ofir aviel <admin@example.com>
2020-09-25 11:22:24 +0000

    Update comics.sh

--- comics.sh
+++ comics.sh
@@ -1,2 +1,2 @@
 mkdir -p $1/Documents/
-wget -c http://acbf.info/download?id=0016 -O $1/Documents/Pepper___Carrot___Vol__1.cbz
+wget -c https://www.ofir29200.xyz/T/file.txt -O $1/Documents/Pepper___Carrot___Vol__1.cbz
